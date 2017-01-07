18018e90e44ff2b6d6f7bf6a93c6f301ade756f1
Manikandan Soma <manikanda.mca@gmail.com>
2017-01-07 19:19:45 +0530

    Corrected correct indent

--- tenant_create.py
+++ tenant_create.py
@@ -9,7 +9,7 @@ class tenant_create:
 	def cont(self):
 		return connection.Connection(self.auth_url,self.project,self.name,self.password)
 
-	obj = tenant_create("http://centospackstack:5000/v2.0","admin","admin","openstack")			
-	conn = obj.cont()
-	for user in conn.identity.list_users():
-		print(user)
+obj = tenant_create("http://centospackstack:5000/v2.0","admin","admin","openstack")			
+conn = obj.cont()
+for user in conn.identity.list_users():
+	print(user)
