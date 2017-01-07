45a2afe7bcb666f4a723c8ca945eaeeac3918f69
Manikandan Soma <manikanda.mca@gmail.com>
2017-01-07 15:27:41 +0530

    Added lines in main_action.py, removed 'name'params from create_stack function

---
+++ main_action.py
@@ -0,0 +1,9 @@
+#from openstack import connection
+class main_action(connect,tenant_action,stack_action):
+	def __init__(self,url,project,name,password):
+        super(connect, self,self.url=url,self.project=project,self.name=name,self.password=password).__init__()
+
+	obj = main_action(auth_url="http://10.144.243.143:5000/v2.0",project_name="tata",username="tata",password="Root@123")
+	conn = obj.con()
+	tenant = obj.create_tenant(conn,self.project)
+	stack = obj.create_stack(conn)
--- stack_action.py
+++ stack_action.py
@@ -1,6 +1,6 @@
 from openstack import connection
 class stack_action:
-	def create_stack(self,conn,name):
-		st = conn.orchestration.create_stack(name)
+	def create_stack(self,conn):
+		st = conn.orchestration.create_stack()
 		st_file = open("/root/heat01.txt")
 
