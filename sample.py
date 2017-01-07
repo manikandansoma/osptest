3004038aeebf097fa63b3b4304135c87e2f9be4b
Manikandan Soma <manikanda.mca@gmail.com>
2017-01-07 12:31:51 +0530

    identity resource removed , compute resource added

--- sample.py
+++ sample.py
@@ -3,5 +3,5 @@ conn = connection.Connection(auth_url="http://centospackstack:5000/v2.0",
                              project_name="tata",
                              username="tata",
                              password="Root@123")
-for user in conn.identity.users():
-   print(user)
+for image in conn.compute.images():
+   print(image)
