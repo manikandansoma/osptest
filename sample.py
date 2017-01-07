44ea32e22ad15651716ed66dd6528512e3c4f4f2
Manikandan Soma <manikanda.mca@gmail.com>
2017-01-07 12:28:42 +0530

    Tata credentials added, admin credentials removed

--- sample.py
+++ sample.py
@@ -1,7 +1,7 @@
 from openstack import connection
 conn = connection.Connection(auth_url="http://centospackstack:5000/v2.0",
-                             project_name="admin",
-                             username="admin",
-                             password="openstack")
-for project in conn.identity.projects():
-   print(project.name)
+                             project_name="tata",
+                             username="tata",
+                             password="Root@123")
+for user in conn.identity.users():
+   print(user)
