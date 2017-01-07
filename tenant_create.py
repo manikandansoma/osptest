5edd5f0f2932b78a7ec105e6e007edcc3e219275
Manikandan Soma <manikanda.mca@gmail.com>
2017-01-07 18:46:44 +0530

    Adding tenant_creat.py

---
+++ tenant_create.py
@@ -0,0 +1,15 @@
+from openstack import connection
+class tenantCreate:
+	def __init__(self,url,project,name,password):
+		self.auth_url=url
+		self.project=project
+		self.name=name
+		self.password=password
+
+	def con(self)
+		return connection.Connection(self.auth_url,self.project=project,self.name=name,self.password=password)
+
+	obj = tenantCreate(auth_url="http://centospackstack:5000/v2.0",project_name="admin",username=""admin",password="openstack")			
+	conn = obj.con()
+	for user in conn.identity.list_users():
+		print(user)
